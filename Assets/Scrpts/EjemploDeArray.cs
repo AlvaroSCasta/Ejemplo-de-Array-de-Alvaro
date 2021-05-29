fe1464f01aefc96c2081e215129886aff49c1576
MochaGreen <mochagreen911@gmail.com>
2021-05-29 10:42:03 -0500

    Initial commit

---
+++ Assets/Scrpts/EjemploDeArray.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploDeArray : MonoBehaviour
+{
+    public GameObject[] balls;
+    private GameObject actualBall;
+
+    public string myText = "Texto Ejmplo";
+    private string gameObjectName;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            actualBall = Instantiate(balls[Random.Range(0, balls.Length)]);
+            gameObjectName = actualBall.name;
+
+            if (gameObjectName != "RedBall(Clone)")
+            {
+                Debug.Log("El objeto es: " + actualBall.name);
+            }
+        }
+
+    }
+}
